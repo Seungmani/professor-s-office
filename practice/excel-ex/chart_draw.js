067428cfa1fd2f903de7448056b3c02ea23867ff
seungmani <dkelcncjs@naver.com>
2023-01-02 22:57:07 +0900

    div 분할 및 정리

--- practice/excel-ex/chart_draw.js
+++ practice/excel-ex/chart_draw.js
@@ -3,9 +3,11 @@ const arr_x = []; // x축
 let draw_data = []; // chart 데이터
 let selected_column; // select 태그에서 선택한 열 이름
 
-//dropdown에서 열 추출 및 x,y축 값 설정
+const $x_axis = document.querySelector('#x-axis');
+
+
+//dropdown에서 열 선택
 function Select() {
-    let k;
     arr = document.getElementById("jsonObject").textContent;
     
     // 선택된 열
@@ -13,18 +15,22 @@ function Select() {
 
     // arr이 문자열이라 json으로
     arr = JSON.parse(arr);
+    
+}
 
-    for (let i = 0; i < arr.length; i++) {
+// x축 지정
+$x_axis.addEventListener('click',()=>{
+    let i=0;
+    for (i = 0; i < arr.length; i++) {
         if (Object.keys(arr[0])[i] === selected_column) {
             break;
         }
     }
-
     // label을 가져옴 
     for (let j = 0; j < arr.length; j++) {
-        arr_x[j] = (Object.values(arr[j])[0]); // x
+        arr_x[j] = (Object.values(arr[j])[i]); // x
     }
-}
+});
 
 // 그래프 삭제
 document.querySelector('#delete').addEventListener('click',()=>{
@@ -38,7 +44,7 @@ document.querySelector('#delete').addEventListener('click',()=>{
     draw_chart();
 });
 
-// 그래프 추가
+// 그래프 추가 == y축
 document.querySelector('#add').onclick = function () {
     // 그래프 색상
     let RGB_1 = Math.floor(Math.random() * (255 + 1));
@@ -50,47 +56,9 @@ document.querySelector('#add').onclick = function () {
         borderColor: 'rgba(' + RGB_1 + ',' + RGB_2 + ',' + RGB_3 + ',0.3)', // 색상
         data: arr.map(arr_y => arr_y[selected_column]), // 데이터
     },)
-
+    console.log(draw_data);
     draw_chart();
 }
-
-
-// 그래프 그리기
-const draw_chart = ()=> {
-    // div 및 화살표 조정
-    $('.grim').css('height', '500px');
-    $('#down').css('display', 'none');
-    $('#up').css('display', 'block');
-
-    // x축 변수들
-    const labels = arr_x;
-
-    // 데이터
-    const data = {
-        labels: labels,
-        datasets: draw_data,
-    };
-    // 설정 값
-    const config = {
-        type: 'line', // 그래프 종류
-        data: data, // 데이터
-        options: {
-            responsive: false, // false=그래프 크기를 css를 이용 지정 가능
-            diplay: 'auto',
-            scales: {
-                xAxes: [{
-                    barThickness: 10,
-                    gridLines: {
-                        display: false
-                    },
-                    offset: true
-                }],
-            }
-        }
-    };
-    // 그리기
-    const myChart = new Chart(document.getElementById('myChart'), config);
-};
 
 // 그래프 초기화
 document.querySelector('#clear').addEventListener('click',() =>{
@@ -124,3 +92,36 @@ document.querySelector('#clear').addEventListener('click',() =>{
     };
     const myChart = new Chart(document.getElementById('myChart'), config);
 });
+
+// 그래프 그리기
+const draw_chart = ()=> {
+
+    // x축 변수들
+    const labels = arr_x;
+
+    // 데이터
+    const data = {
+        labels: labels,
+        datasets: draw_data,
+    };
+    // 설정 값
+    const config = {
+        type: 'line', // 그래프 종류
+        data: data, // 데이터
+        options: {
+            responsive: false, // false=그래프 크기를 css를 이용 지정 가능
+            diplay: 'auto',
+            scales: {
+                xAxes: [{
+                    barThickness: 10,
+                    gridLines: {
+                        display: false
+                    },
+                    offset: true
+                }],
+            }
+        }
+    };
+    // 그리기
+    const myChart = new Chart(document.getElementById('myChart'), config);
+};
